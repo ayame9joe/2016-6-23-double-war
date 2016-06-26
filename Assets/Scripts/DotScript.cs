1a703c8a5923bfada58b3ed4c38c4a13916e31c8
Qiao Xiaomeng <ayame9joe@gmail.com>
2016-06-26 22:49:31 +0800

    project built, dots and players movement

---
+++ Assets/Scripts/DotScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DotScript : MonoBehaviour {
+
+	Vector3 speed;
+
+	float maxSpeed = 0.1f;
+
+	int directionChangeTimes = 0;
+
+	// Use this for initialization
+	void Start () {
+		speed = new Vector3 (Random.Range (-maxSpeed, maxSpeed), 
+			Random.Range (-maxSpeed, maxSpeed), 0);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (directionChangeTimes % 2 == 0) {
+			this.transform.Translate (speed);
+		} else {
+			this.transform.Translate (new Vector3 (-speed.x, -speed.y, 0));
+		}
+
+		if (Vector3.Distance (this.transform.position, new Vector3 (0, 0, 0)) > Defines.width / 2) {
+
+			directionChangeTimes++;
+			//Debug.Log (changeDirection);
+
+		}
+	
+	
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject dot;
+
+	public GameObject[] players = new GameObject[2];
+
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i <= Defines.numberOfDots; i++) {
+
+			float tempRangeX = Random.Range (-Defines.width / 2, Defines.width / 2);
+			float tempRangeY = Random.Range(-Defines.height / 2, Defines.height / 2);
+
+			GameObject.Instantiate (dot, new Vector3 (tempRangeX, tempRangeY, 0), Quaternion.identity);
+
+
+			//Debug.Log (tempRangeX + Defines.minRangeX + "/" + tempRangeY );
+			//Debug.Log(tempRangeY);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.touchCount > 0) {
+			for (int i = 0; i < players.Length; i++) {
+				if (Input.touches [1].phase == TouchPhase.Moved) {
+					players[i].transform.position = Camera.main.ScreenToWorldPoint(Input.touches [i].position);
+				}
+			}
+		}
+	
+	}
+}
